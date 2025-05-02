2648eca17a540d324f14dc21bad7d67443ad37c2
ks <erikjames1415@gmail.com>
2025-05-03 00:28:24 +0500

    project

--- Code/HostelManagement/src/hostelmanagement/HostelManagement.java
+++ Code/HostelManagement/src/hostelmanagement/HostelManagement.java
@@ -5,6 +5,14 @@ public class HostelManagement {
 
     public static void main(String[] args) {
         
+          HostelManagement system = new HostelManagement();
+        system.run();
     }
+    public void run() {
+        // Sample menu simulation for administrators
+        System.out.println("Welcome to Hostel Room Management System - Administrator Module");
+        // Add CLI or UI handler here
+    }
+    
     
 }

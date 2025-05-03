5a4b86a75746aae6aa0a7caf058216059b422d60
Nouman Malik <inoumanmalik@outlook.com>
2025-05-03 09:02:23 +0500

    Added pdf and code for manage room

--- Code/HostelManagement/src/hostelmanagement/HostelManagement.java
+++ Code/HostelManagement/src/hostelmanagement/HostelManagement.java
@@ -1,10 +1,15 @@
 
 package hostelmanagement;
 
+import javax.swing.SwingUtilities;
+
 public class HostelManagement {
 
     public static void main(String[] args) {
-        
+         SwingUtilities.invokeLater(() -> {
+            RoomManager roomManager = new RoomManager();
+            new RoomManagementGUI(roomManager).setVisible(true);
+        });
     }
     
 }

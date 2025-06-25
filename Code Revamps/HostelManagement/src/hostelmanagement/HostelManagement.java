c7fd429dd939516e2154d51b8e10c5cc16fe1e0c
Nouman Malik <inoumanmalik@outlook.com>
2025-06-25 13:02:58 +0500

    Changed word to pdf file

--- Code Revamps/HostelManagement/src/hostelmanagement/HostelManagement.java
+++ Code Revamps/HostelManagement/src/hostelmanagement/HostelManagement.java
@@ -9,10 +9,10 @@ public class HostelManagement {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-             MainMenuGUI mainMenu = new MainMenuGUI();
-             mainMenu.setVisible(true);
-//            UserManager userManager = new UserManager();
-//            new LoginGUI(userManager).setVisible(true);
+//             MainMenuGUI mainMenu = new MainMenuGUI();
+//             mainMenu.setVisible(true);
+            UserManager userManager = new UserManager();
+            new LoginGUI(userManager).setVisible(true);
         });
     }
 }

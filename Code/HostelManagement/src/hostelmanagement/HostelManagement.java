23653a200dac3896c9aa325ce469fa61b56d8060
Nouman Malik <inoumanmalik@outlook.com>
2025-06-27 16:03:45 +0500

    Added Video Folder

--- Code/HostelManagement/src/hostelmanagement/HostelManagement.java
+++ Code/HostelManagement/src/hostelmanagement/HostelManagement.java
@@ -1,4 +1,6 @@
 
+
+
 package hostelmanagement;
 
 import javax.swing.SwingUtilities;
@@ -6,9 +8,11 @@ import javax.swing.SwingUtilities;
 public class HostelManagement {
 
     public static void main(String[] args) {
-         SwingUtilities.invokeLater(() -> {
-            RoomManager roomManager = new RoomManager();
-            new RoomManagementGUI(roomManager).setVisible(true);
+        SwingUtilities.invokeLater(() -> {
+//             MainMenuGUI mainMenu = new MainMenuGUI();
+//             mainMenu.setVisible(true);
+            UserManager userManager = new UserManager();
+            new LoginGUI(userManager).setVisible(true);
         });
-    }  
+    }
 }
